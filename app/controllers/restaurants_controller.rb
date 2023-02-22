5e58d70262cef3847f3ac03a1b335e1a3c4b1cb6
Eva Touboulic <etouboulic@gmail.com>
2023-02-22 12:50:10 +0100

    All done and all green

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,8 +10,11 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(params_restaurant)
-    @restaurant.save
-    redirect_to restaurants_path
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
