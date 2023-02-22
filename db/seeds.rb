1be14be672b670dc88483ce46a1509f57ceab6ec
Eva Touboulic <etouboulic@gmail.com>
2023-02-22 12:29:31 +0100

    All done without style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Review.destroy_all
+Restaurant.destroy_all
+
+puts 'Generate new restaurants ...'
+restaurant0 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.city, phone_number: Faker::PhoneNumber.phone_number, category: "chinese")
+restaurant0.save
+restaurant1 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.city, phone_number: Faker::PhoneNumber.phone_number, category: "italian")
+restaurant1.save
+restaurant2 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.city, phone_number: Faker::PhoneNumber.phone_number, category: "japanese")
+restaurant2.save
+restaurant3 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.city, phone_number: Faker::PhoneNumber.phone_number, category: "french")
+restaurant3.save
+restaurant4 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.city, phone_number: Faker::PhoneNumber.phone_number, category: "italian")
+restaurant4.save
+
+puts "Finish !"
